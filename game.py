7d1d99788435027a442a419e711b68b47d5b0631
K <ktzy0305@gmail.com>
2020-04-14 13:13:46 +0800

    Cleaned up codes

--- game.py
+++ game.py
@@ -4,30 +4,22 @@ import turtle
 from threading import Timer
 
 # Fixed Variables:
-BASE_DIR = os.getcwd()
 NUMBER_OF_PLAYERS = 3
-PUZZLE_DEFAULT =  "GET THE FRUSTRATION OUT OF YOUR SYSTEM"
 VOWELS = ["A", "E", "I", "O", "U"]
-# FONT = "Calibri"
-FONT = "Century Gothic"
-# BACKGROUND_COLOR = "#ffc4e4"
-BACKGROUND_COLOR = "#40e0d0"
 
-# Screen Set Up
-window = turtle.Screen()
-window.title("Wheel Of Fortune")
-window.setup(width=1.0, height=1.0)
-turtle.bgcolor(BACKGROUND_COLOR)
-turtle.speed(0)
-turtle.penup()
+# Fixed Positions
+TITLE_POSITION = (0, 350)
 
-# Write Welcome
-turtle.pencolor("white")
-turtle.setpos(0, 350)
-turtle.write("WHEEL OF FORTUNE", align="center", font=(FONT, 30, "bold"))
-turtle.penup()
-turtle.hideturtle()
+# Display Asthethics:
+FONT = "Calibri"
+BACKGROUND_COLOR = "#ffc4e4"
+ARROW_COLOR = "#090057"
 
+# Window Set Up
+wn = turtle.Screen()
+wn.title("Wheel Of Fortune")
+wn.setup(width=1.0, height=1.0)
+wn.bgcolor(BACKGROUND_COLOR)
 
 # Application Functions
 def load_phrases(file):
@@ -48,27 +40,46 @@ def findnth(haystack, needle, n):
         return -1
     return len(haystack)-len(parts[-1])-len(needle)
 
+def display_title():
+    title = turtle.Turtle()
+    title.speed(0)
+    title.penup()
+    title.pencolor("white")
+    title.setpos(TITLE_POSITION)
+    title.write("WHEEL OF FORTUNE", align="center", font=(FONT, 30, "bold"))
+    title.hideturtle()
+
 
 # Objects For Wheel Of Fortune
 class Game:
     def __init__(self):
-        self.players = []
-        self.phrases = load_phrases(os.path.join(BASE_DIR, "WofFPhrases.txt"))
-        self.wheel = Wheel(radius=250)
-        self.puzzle = None
         self.current_player_number = 1
-        self.results_turtle = turtle.Turtle()
-        self.results_turtle.speed(0)
+        self.phrases = load_phrases(os.path.join(os.getcwd(), "WofFPhrases.txt"))
+        self.players = []
+        self.puzzle = None
+        self.results = turtle.Turtle()
+        self.results.speed(0)
+        self.results.hideturtle()
+        self.wheel = Wheel(radius=250)
 
-    def find_player(self, player_number):
+    def display_game_results(self):
+        x_pos, y_pos = 400, 350
+        self.results.reset()
+        self.results.penup()
+        self.results.color("black")
         for player in self.players:
-            if player.player_number == player_number:
-                return player
+            self.results.goto(x_pos,y_pos)
+            self.results.write("{0} - ${1}".format(player.name, player.money), font=(FONT, 20, 'bold'))
+            y_pos -= 30
+        self.results.hideturtle()
 
     def game_results(self):
-        self.render_game_results()
+        self.display_game_results()
         for player in self.players:
-            print("Player {0}: {1} - ${2}".format(player.player_number, player.name, player.money))
+            print("{0} - ${1}".format(player.name, player.money))
+
+    def get_player(self, player_number):
+        return list(filter(lambda player: player.player_number == player_number, self.players))[0]
     
     def next_player(self):
         if self.current_player_number == NUMBER_OF_PLAYERS: 
@@ -79,12 +90,12 @@ class Game:
     def play(self):
         self.puzzle = Puzzle(random.choice(self.phrases))
         while not self.puzzle.is_solved():
-            player = self.find_player(self.current_player_number)
+            player = self.get_player(self.current_player_number)
             self.puzzle.display()
             print()
             self.game_results()
             print("\n{}'s Turn\n".format(player.name))
-            print("\nSpinning The Wheel...\n")
+            print("\nSpinning...\n")
             outcome = self.wheel.spin()
 
             if outcome == "Bankrupt":
@@ -96,14 +107,13 @@ class Game:
                 self.next_player()
             else:
                 print("${}".format(outcome))
-                # Start Passive Timer
-                t = Timer(10.0, print, ['Sorry, times up!'])
-                t.start()
+                timer = Timer(10.0, print, ['Times up! You lost your turn.'])
+                timer.start()
                 letter = ""
                 valid_input = False
                 while not valid_input:
                     letter = input("Guess a letter, you have 10 seconds: ")
-                    if t.is_alive():
+                    if timer.is_alive():
                         if letter == "":
                             print("Letter cannot be empty!")
                         elif len(letter) > 1:
@@ -113,7 +123,6 @@ class Game:
                                 if letter in self.puzzle.list_form:
                                     print("{} already found in the puzzle!".format(letter))
                                 else:   
-                                    # If letter is vowel deduct $250
                                     if is_a_vowel(letter):
                                         if outcome == "Free Play":
                                             print("{0} used Free Play for the vowel '{1}'".format(player.name, letter.upper()))
@@ -131,17 +140,16 @@ class Game:
                                 print("Please enter a valid alphabet!")
                     else:
                         valid_input = True
-                t.cancel()
+                timer.cancel()
                 
-                # Check if passive timer exceeded 10 seconds
-                if t.is_alive():
-                    del t # Delete the timer instance for memory management
-                    if self.puzzle.check(letter):
-                        number_of_letters = self.puzzle.count_letters(letter)
-                        print("You've guessed it! There are {0} '{1}'(s)".format(number_of_letters, letter))
+                if timer.is_alive():
+                    del timer 
+                    if self.puzzle.check_letter_exists(letter):
+                        occurrences = self.puzzle.count_letter_occurrence(letter)
+                        print("You've guessed it! There are {0} '{1}'(s)".format(occurrences, letter))
                         if not outcome == "Free Play" and not is_a_vowel(letter):
-                            player.money += (number_of_letters * outcome)
-                        self.puzzle.fill_letter(letter)
+                            player.money += (occurrences * outcome)
+                        self.puzzle.fill(letter)
                         self.puzzle.display()
                         self.game_results()
                         if not self.puzzle.is_solved():
@@ -171,20 +179,10 @@ class Game:
                         if not outcome == "Free Play":
                             self.next_player()
                 else:
-                    del t # Delete the timer instance for memory management
+                    del timer
                     self.next_player()
         print("\nFinal Results")
         self.game_results()
-
-    def render_game_results(self):
-        x_pos, y_pos = 400, 350
-        self.results_turtle.reset()
-        self.results_turtle.penup()
-        self.results_turtle.color("black")
-        for player in self.players:
-            self.results_turtle.goto(x_pos,y_pos)
-            self.results_turtle.write("Player {0}: {1} - ${2}".format(player.player_number, player.name, player.money), font=(FONT, 20, 'bold'))
-            y_pos -= 30
 
     def setup(self):
         print("\nPlayers, please enter your name.\n")
@@ -194,7 +192,7 @@ class Game:
             name = ""
             while not valid_input:
                 print("Player {}:".format(player_number))
-                name = input("Enter a name: ")
+                name = input("Enter your name: ")
                 if name != "":
                     valid_input = True
                 else:
@@ -210,25 +208,29 @@ class Game:
 class Wheel:
     def __init__(self,  radius):
         self.radius = radius
+        self.current_sector = 0
+        self.arrow = turtle.Turtle()
         self.sectors = [550, 800, 300, 700, 900, 500, 5000, 'Bankrupt',
                         300, 500, 450, 500, 800, 'Lose A Turn', 700, 'Free Play',
                         650, 'Bankrupt', 600, 500, 350, 600, 500, 400]
         self.colors = ['#d40ae1', '#ffa700', '#32d0ff', '#ff4141', '#fdff0d', '#5cc30c', '#dedede', '#000000', 
                        '#ffa700', '#5cc30c', '#ff79bc', '#d40ae1', '#ff4141', '#ffffff', '#32d0ff', '#009619',
                        '#d40ae1', '#000000', '#ff79bc', '#5cc30c', '#32d0ff', '#ff4141', '#ff79bc', '#fdff0d']
-        self.current_sector = 0
-        self.arrow_turtle = turtle.Turtle()
-        self.arrow_turtle.speed(0)
-        self.arrow_turtle.hideturtle()
-        self.render()
+        self.x_adjustments = [-40, -45, -50, -50, -50, -50, -45, -50, -30, -20, -10, 0,
+                              10, 10, 25, 15, 20, 10, 15, 10, 5, -5, -20, -30]
+        self.y_adjustments = [20, 15, 10, 0, -10, -20, -30, -35, -40, -45, -45, -45,
+                              -40, -30, -25, -10, -10, 0, 10, 20, 20, 25, 30, 30]
+        self.arrow.speed(0)
+        self.arrow.hideturtle()
+        self.display_on_screen()
 
-    def render(self, center=(0, 80)):
+    def display_on_screen(self, center=(0, 80)):
         wheel_turtle = turtle.Turtle()
         wheel_turtle.speed(0)
 
-        wheel_text_turtle = turtle.Turtle()
-        wheel_text_turtle.speed(0)
-        wheel_text_turtle.penup()
+        sector_text = turtle.Turtle()
+        sector_text.speed(0)
+        sector_text.penup()
 
         slice_angle = 360 / len(self.sectors)
         heading, position = 90, (center[0] + self.radius, center[1])
@@ -240,87 +242,26 @@ class Wheel:
             wheel_turtle.setheading(heading)
             wheel_turtle.pendown()
             wheel_turtle.begin_fill()
-
-            # Fix Alignment Of Text (Need To Hard Code)
-            if count == 0:
-                wheel_text_turtle.setposition(position[0] - 40, position[1]+20)
-            elif count == 1:
-                wheel_text_turtle.setposition(position[0] - 45, position[1]+15)
-            elif count == 2:
-                wheel_text_turtle.setposition(position[0] - 50, position[1]+10)
-            elif count == 3:
-                wheel_text_turtle.setposition(position[0] - 50, position[1])
-            elif count == 4:
-                wheel_text_turtle.setposition(position[0] - 50, position[1]-10)    
-            elif count == 5:
-                wheel_text_turtle.setposition(position[0] - 50, position[1]-20)    
-            elif count == 6:
-                wheel_text_turtle.setposition(position[0] - 45, position[1]-30)
-            elif count == 7:
-                wheel_text_turtle.setposition(position[0] - 55, position[1]-35)
-            elif count == 8:
-                wheel_text_turtle.setposition(position[0] - 30, position[1]-40)
-            elif count == 9:
-                wheel_text_turtle.setposition(position[0] - 20, position[1]-45)    
-            elif count == 10:
-                wheel_text_turtle.setposition(position[0] - 10, position[1]-45)    
-            elif count == 11:
-                wheel_text_turtle.setposition(position[0], position[1]-45)
-            elif count == 12:
-                wheel_text_turtle.setposition(position[0] + 10, position[1]-40)
-            elif count == 13:
-                wheel_text_turtle.setposition(position[0] + 10, position[1]-30)
-            elif count == 14:
-                wheel_text_turtle.setposition(position[0] + 25, position[1]-25)
-            elif count == 15:
-                wheel_text_turtle.setposition(position[0] + 10, position[1]-10)    
-            elif count == 16:
-                wheel_text_turtle.setposition(position[0] + 20, position[1]-10)    
-            elif count == 17:
-                wheel_text_turtle.setposition(position[0] + 5, position[1])  
-            elif count == 18:
-                wheel_text_turtle.setposition(position[0] + 15, position[1]+10)
-            elif count == 19:
-                wheel_text_turtle.setposition(position[0] + 10, position[1]+20)
-            elif count == 20:
-                wheel_text_turtle.setposition(position[0] + 5, position[1]+20)
-            elif count == 21:
-                wheel_text_turtle.setposition(position[0] - 5, position[1]+25)    
-            elif count == 22:
-                wheel_text_turtle.setposition(position[0] - 20, position[1]+30)    
-            elif count == 23:
-                wheel_text_turtle.setposition(position[0] - 30, position[1]+30)  
-            else:
-                wheel_text_turtle.setposition(position[0], position[1])
-
-            wheel_text_turtle.pencolor("black")
+            sector_text.setposition(position[0] + self.x_adjustments[count], position[1] + self.y_adjustments[count])
+            sector_text.pencolor("black")
             if isinstance(self.sectors[count], int):
-                wheel_text_turtle.write("${}".format(self.sectors[count]), font=(FONT, 10, "bold"))
+                sector_text.write("${}".format(self.sectors[count]), font=(FONT, 10, "bold"))
             else:
                 if self.sectors[count] == "Bankrupt" or self.sectors[count] == "Free Play":
-                     wheel_text_turtle.pencolor("white")
-                wheel_text_turtle.write(self.sectors[count], font=(FONT, 10, "bold"))
+                     sector_text.pencolor("white")
+                sector_text.write(self.sectors[count], font=(FONT, 10, "bold"))
 
             wheel_turtle.circle(self.radius, extent=slice_angle)
             heading, position = wheel_turtle.heading(), wheel_turtle.position()
             wheel_turtle.penup()
-            wheel_text_turtle.penup()
+            sector_text.penup()
             wheel_turtle.goto(center)
             wheel_turtle.end_fill()
     
         wheel_turtle.hideturtle()
-        wheel_text_turtle.hideturtle()
+        sector_text.hideturtle()
     
-        # Render Arrow
-        self.arrow_turtle.goto(center)
-        self.arrow_turtle.left(0.5*slice_angle)
-        self.arrow_turtle.color("#6e6e6e")
-        self.arrow_turtle.pensize(10)
-        self.arrow_turtle.pendown()
-        self.arrow_turtle.forward(self.radius - 75)
-        self.arrow_turtle.penup()
-        self.arrow_turtle.hideturtle()
-
+        self.spin_to(self.current_sector)
 
     def spin(self):
         index = random.randrange(0, len(self.sectors))
@@ -334,76 +275,60 @@ class Wheel:
             current = self.current_sector
             for count in range(number_of_rotations):
                 current += 1
-                self.arrow_turtle.reset()
-                self.arrow_turtle.speed(0)
-                self.arrow_turtle.penup()
-                self.arrow_turtle.goto(center)
-                self.arrow_turtle.left((0.5*slice_angle) + (current%24)*slice_angle)
-                self.arrow_turtle.color("#6e6e6e")
-                self.arrow_turtle.pensize(10)
-                self.arrow_turtle.pendown()
-                self.arrow_turtle.forward(self.radius - 75)
-                self.arrow_turtle.penup()
-                self.arrow_turtle.hideturtle()
-            
-        for count in range(self.current_sector, target+1):
-            self.arrow_turtle.reset()
-            self.arrow_turtle.penup()
-            self.arrow_turtle.goto(center)
-            self.arrow_turtle.left((0.5*slice_angle) + count*slice_angle)
-            self.arrow_turtle.color("#6e6e6e")
-            self.arrow_turtle.pensize(10)
-            self.arrow_turtle.pendown()
-            self.arrow_turtle.forward(self.radius - 75)
-            self.arrow_turtle.penup()
-            self.arrow_turtle.hideturtle()
+                self.arrow.reset()
+                self.arrow.speed(0)
+                self.arrow.penup()
+                self.arrow.goto(center)
+                self.arrow.left((0.5*slice_angle) + (current%24)*slice_angle)
+                self.arrow.color(ARROW_COLOR)
+                self.arrow.pensize(8)
+                self.arrow.pendown()
+                self.arrow.forward(self.radius - 75)
+                self.arrow.penup()
+                self.arrow.hideturtle()
+        else:
+            for count in range(self.current_sector, target+1):
+                self.arrow.reset()
+                self.arrow.speed(0)
+                self.arrow.penup()
+                self.arrow.goto(center)
+                self.arrow.left((0.5*slice_angle) + count*slice_angle)
+                self.arrow.color(ARROW_COLOR)
+                self.arrow.pensize(10)
+                self.arrow.pendown()
+                self.arrow.forward(self.radius - 75)
+                self.arrow.penup()
+                self.arrow.hideturtle()
         self.current_sector = target
 
 class Puzzle:
     def __init__(self, phrase):
-        self.list_form = []
-        self.turtles = []
         self.phrase = phrase
         self.solved = False
+        self.list_form = []
+        self.turtles = []
         self.generate_list_form()
 
-    def count_letters(self, alphabet):
+    def count_letter_occurrence(self, alphabet):
         count = 0
         for letter in self.phrase:
             if letter == alphabet.upper():
                 count += 1
         return count
 
-    def check(self, alphabet):
+    def check_letter_exists(self, alphabet):
         return alphabet.upper() in self.phrase
     
     def display(self):
-        self.render()
+        self.display_on_screen()
         print("".join(self.list_form))
 
-    def fill_letter(self, alphabet):
-        for count, letter in enumerate(self.phrase):
-            if alphabet.upper() == letter:
-                self.list_form[count] = letter
-        if "".join(self.list_form) == self.phrase:
-            self.solved = True
-
-    def is_solved(self):
-        return self.solved
-
-    def generate_list_form(self):
-        for character in self.phrase:
-            if character == " ":
-                self.list_form.append(" ")
-            else:
-                self.list_form.append("_")
-    
-    def render(self):
+    def display_on_screen(self):
+        self.turtles = []
         x_pos, y_pos = -650, -200
         space_count =  0
         for count, character in enumerate(self.list_form):
             if character == " ":
-                # Bring over long words into next row
                 space_count += 1
                 next_space = findnth(self.phrase," ",space_count)
                 word = ""
@@ -446,10 +371,23 @@ class Puzzle:
             if x_pos >= 650:
                 x_pos = -650
                 y_pos -= 60
-        # for letter in self.list_form:
-        #     letter_turtle = turtle.Turtle 
 
-        # pass
+    def fill(self, alphabet):
+        for count, letter in enumerate(self.phrase):
+            if alphabet.upper() == letter:
+                self.list_form[count] = letter
+        if "".join(self.list_form) == self.phrase:
+            self.solved = True
+
+    def is_solved(self):
+        return self.solved
+
+    def generate_list_form(self):
+        for character in self.phrase:
+            if character == " ":
+                self.list_form.append(" ")
+            else:
+                self.list_form.append("_")
 
     def solve(self, phrase):
         if self.phrase == phrase.upper():
@@ -460,8 +398,8 @@ class Player:
         self.name = name
         self.money = 0
         self.player_number = player_number
-        self.round_amounts = []
 
 if __name__ == "__main__":
+    display_title()
     game = Game()
     game.start()
